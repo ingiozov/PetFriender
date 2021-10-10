12e04671923a210a600aac407e1ebf7e3c96d8e0
ingiozov <ingiozov@noreply.github.com>
2021-10-11 00:42:04 +0300

    fetch

--- pet-friender-frontend/pages/index.js
+++ pet-friender-frontend/pages/index.js
@@ -1,9 +1,23 @@
 import Layout from '../components/Layout'
+import { API_URL } from '../config'
 
-export default function HomePage() {
+export default function HomePage({ dogs }) {
+  console.log(dogs)
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Pets Available for Adoption Nearby</h1>
+      {dogs.map((dog) => (
+        <h2>{dog.name}</h2>
+      ))}
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/dogs`)
+  const dogs = await res.json()
+
+  return {
+    props: { dogs },
+  }
 }
